06a479efa8c6c7b37f22627b2ab947753b2e5d8c
Basit Shehram <basit@frequency.com>
2015-07-08 09:19:25 -0700

    updated

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,18 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+execute "what am i" do
+time = Time.now.to_i  
+  cwd '/tmp'
+  command "mkdir -p cb_dev_www_nodejs_#{time}"
+  action :run
+end
+
+bash 'update and install components' do
+  code <<-"EOH"
+    /usr/bin/yum -y update
+  EOH
+  action :run
+end
+
